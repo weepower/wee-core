2fee7c9858b0af8c27efcf9f81e393b9978373e8
Michael Leigeber <michael@caddis.co>
2015-07-18 17:09:44 -0500

    Add initial addEvent functionality to register custom events Resolve bug with removing namespaced events

--- script/wee.events.js
+++ script/wee.events.js
@@ -125,16 +125,29 @@
 		trigger: function(target, event) {
 			W.$each(target, function(el) {
 				if (W._doc.createEvent) {
-					var ev = W._doc.createEvent('HTMLEvents');
+					var evt = W._doc.createEvent('HTMLEvents');
 
-					ev.initEvent(event, true, false);
-					el.dispatchEvent(ev);
+					evt.initEvent(event, true, false);
+					el.dispatchEvent(evt);
 				} else {
 					el.fireEvent('on' + event);
 				}
 			});
+		},
+
+		/**
+		 * Add a custom event
+		 *
+		 * @param {string} name
+		 * @param {function} on
+		 * @param {function} off
+		 */
+		addEvent: function(name, on, off) {
+			this.$private.custom[name] = [on, off];
 		}
 	}, {
+		custom: {},
+
 		/**
 		 * Attach specific event logic to element
 		 *
@@ -142,13 +155,13 @@
 		 * @param {object} obj
 		 * @param {object} c
 		 */
-		bind: function(els, obj, c) {
+		bind: function(els, obj, opt) {
 			var scope = this;
 
 			// Redefine variables when delegating
-			if (c && c.delegate) {
-				c.targ = els;
-				els = c.delegate;
+			if (opt && opt.delegate) {
+				opt.targ = els;
+				els = opt.delegate;
 			}
 
 			// For each element attach events
@@ -161,23 +174,17 @@
 					for (; i < evts.length; i++) {
 						var conf = W.$extend({
 								args: [],
-								one: false,
+								once: false,
 								scope: el
-							}, c),
+							}, opt),
 							fn = obj[key],
 							evt = evts[i],
 							ev = evt,
 							f = fn;
 						evt = evt.split('.')[0];
 
-						if (evt == 'mouseenter' || evt == 'mouseleave') {
-							conf.args.unshift(fn);
-
-							fn = scope.mouseEvent;
-							evt = 'mouse' + ((evt == 'mouseenter') ? 'over' : 'out');
-
-							conf.args.unshift(0, c && c.targ ? c.targ : el);
-						} else {
+						// Prepend element to callback arguments if necessary
+						if (conf.args[1] !== el) {
 							conf.args.unshift(0, el);
 						}
 
@@ -200,7 +207,8 @@
 
 								conf.args[0] = e;
 
-								// If watch within parent make sure the target matches the selector
+								// If watch within parent make sure the target
+								// matches the selector
 								if (conf.targ) {
 									var targ = conf.targ,
 										sel = targ._$ ? targ.sel : targ;
@@ -225,26 +233,33 @@
 								if (cont) {
 									W.$exec(fn, conf);
 
-									// If the event is to be executed once unbind it immediately
+									// If the event is to be executed once
+									// unbind it immediately
 									if (conf.once) {
 										scope.$public.off(el, evt, f);
 									}
 								}
 							};
 
-							// Ensure the specified element, event, and function combination hasn't already been bound
+							// Ensure the specified element, event, and function
+							// combination hasn't already been bound
 							if (evt != 'init' && scope.$public.bound(el, ev, f).length < 1) {
-								W._legacy ?
-									el.attachEvent('on' + evt, cb) :
-									el.addEventListener(evt, cb);
+								// Determine if the event is native or custom
+								if ('on' + evt in W._doc) {
+									W._legacy ?
+										el.attachEvent('on' + evt, cb) :
+										el.addEventListener(evt, cb);
 
-								scope.$push('evts', {
-									el: el,
-									ev: ev,
-									evt: evt,
-									cb: cb,
-									fn: f
-								});
+									scope.$push('evts', {
+										el: el,
+										ev: ev,
+										evt: evt,
+										cb: cb,
+										fn: f
+									});
+								} else if (scope.custom[evt]) {
+									scope.custom[evt][0](el, fn, conf);
+								}
 							}
 
 							if (evt == 'init' || conf.init === true) {
@@ -257,40 +272,6 @@
 		},
 
 		/**
-		 * Ensure mouse has actually entered or left root element before firing event
-		 *
-		 * @param {event} e
-		 * @param {HTMLElement} parent
-		 * @param {function} fn
-		 */
-		mouseEvent: function(e, parent, fn) {
-			var child = e.relatedTarget,
-				checkParent = function(parent, child) {
-					if (parent !== child) {
-						while (child && child !== parent) {
-							child = child.parentNode;
-						}
-
-						return child === parent;
-					}
-
-					return false;
-				};
-
-			if (child === parent || checkParent(parent, child)) {
-				return;
-			}
-
-			var args = W._slice.call(arguments);
-			args.splice(2, 1);
-
-			W.$exec(fn, {
-				args: args,
-				scope: this
-			});
-		},
-
-		/**
 		 * Detach event(s) from element
 		 *
 		 * @param {(HTMLElement|string)} [sel]
@@ -298,17 +279,21 @@
 		 * @param {function} [fn]
 		 */
 		off: function(sel, evt, fn) {
+			var scope = this;
+
 			W.$each(this.$public.bound(sel, evt, fn), function(e) {
-				W._legacy ?
-					e.el.detachEvent('on' + e.evt, e.cb) :
-					e.el.removeEventListener(e.evt, e.cb);
+				if ('on' + e.evt in W._doc) {
+					W._legacy ?
+						e.el.detachEvent('on' + e.evt, e.cb) :
+						e.el.removeEventListener(e.evt, e.cb);
 
-				// Remove object from the bound array
-				var bound = this.$get('evts');
+					// Remove object from the bound array
+					var bound = scope.$get('evts');
 
-				bound.splice(bound.indexOf(e), 1);
-			}, {
-				scope: this
+					bound.splice(bound.indexOf(e), 1);
+				} else if (scope.custom[evt]) {
+					scope.custom[evt][1](e.el, e.cb);
+				}
 			});
 		}
 	});
