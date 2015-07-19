8027a7805f874038f89d23967eb2f077291a056a
Michael Leigeber <michael@caddis.co>
2015-07-19 14:11:33 -0500

    Move the intern testing package to the development dependencies

--- script/tests/config.js
+++ script/tests/config.js
@@ -1,7 +1,7 @@
 define({
 	proxyPort: 9010,
 	proxyUrl: 'http://localhost:9010/',
-	excludeInstrumentation: /^(?:bower_components|node_modules|tests)\//,
+	excludeInstrumentation: /^(?:bower_components|node_modules)\//,
 	tunnel: 'NullTunnel',
 	loader: {
 		packages: [
