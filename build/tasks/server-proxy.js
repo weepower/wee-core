0c0754aa1d785e6fd1c33823817967b33f6a5250
Michael Leigeber <michael@caddis.co>
2015-07-21 20:43:07 -0500

    Move injection code to work on local Grunt task Update version

--- build/tasks/server-proxy.js
+++ build/tasks/server-proxy.js
@@ -1,4 +1,4 @@
-/* global module, project, server */
+/* global module, project, server, Wee */
 
 module.exports = function(grunt) {
 	grunt.registerTask('proxy', function() {
@@ -9,6 +9,44 @@ module.exports = function(grunt) {
 			if (project.server.host !== 'auto') {
 				server.host = project.server.host;
 			}
+		}
+
+		// Server asset injection
+		if (project.server.inject && project.server.inject.length) {
+			var inject = [];
+
+			project.server.inject.forEach(function(file) {
+				if (file.slice(-3) == '.js') {
+					inject.push('<script src="' + file + '"></script>');
+					Wee.serverWatch(file);
+				} else if (file.slice(-4) == '.css') {
+					inject.push('<link rel="stylesheet" href="' + file + '">');
+					Wee.serverWatch(file);
+				}
+			});
+
+			server.middleware = function(req, res, next) {
+				if (req.headers && req.headers.accept.match(/^text\/html/)) {
+					var _write = res.write;
+
+					res.setHeader(
+						'Cache-Control',
+						'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0'
+					);
+
+					res.write = function(data) {
+						_write.call(
+							res,
+							data.toString().replace(
+								'</head>',
+								inject.join('') + '</head>'
+							)
+						);
+					};
+				}
+
+				next();
+			};
 		}
 
 		// Secure mode
