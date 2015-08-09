b4755ff432b8f8c78d4f49259ad646a7d8680f24
Michael Leigeber <michael@caddis.co>
2015-08-09 10:41:58 -0500

    Add alternate image extensions webp and bmp to core sync script

--- build/tasks/core-sync.js
+++ build/tasks/core-sync.js
@@ -1,36 +1,60 @@
 /* global config, fs */
 
 module.exports = function(grunt) {
+	var glob = require('glob');
+
 	grunt.registerTask('syncDirectory', function(task) {
+		var corePath,
+			destPath,
+			modulePath,
+			extensions,
+			files;
+
 		if (task === 'fonts') {
-			var glob = require('glob'),
-				files;
+			corePath = config.paths.fontSource;
+			destPath = config.paths.font;
+			modulePath = 'fonts';
+			extensions = 'eot,svg,ttf,woff,woff2';
+		} else if (task === 'images') {
+			corePath = config.paths.imgSource;
+			destPath = config.paths.img;
+			modulePath = 'img';
+			extensions = 'bmp,webp';
+		}
 
-			// Sync core fonts
-			fs.removeSync(config.paths.font);
+		if (corePath) {
+			// Sync core files
+			fs.removeSync(destPath);
 
-			files = glob.sync(config.paths.fontSource + '**/*.{eot,svg,ttf,woff,woff2}');
+			files = glob.sync(corePath + '**/*.{' + extensions + '}');
 
 			files.forEach(function(file) {
-				var name = file.replace(config.paths.fontSource, '');
+				var name = file.replace(corePath, '');
 
-				fs.copySync(file, config.paths.font + name);
+				fs.copySync(file, destPath + name);
 			});
 
-			// Sync module fonts
-			var directories = glob.sync(config.paths.modulesSource + 'fonts');
+			// Sync module files
+			var directories = glob.sync(config.paths.modulesSource + modulePath);
 
 			directories.forEach(function(directory) {
 				fs.removeSync(directory);
 			});
 
-			files = glob.sync(config.paths.modulesSource + '*/fonts/**/*.{eot,svg,ttf,woff,woff2}');
+			files = glob.sync(
+				config.paths.modulesSource +
+				'*/' + modulePath + '/**/*.{' + extensions + '}'
+			);
 
 			files.forEach(function(file) {
 				var name = file.replace(config.paths.modulesSource, '');
 
 				fs.copySync(file, config.paths.modules + name);
 			});
+
+			if (task === 'images') {
+				grunt.task.run('imagemin');
+			}
 		}
 	});
 };
