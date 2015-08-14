dd459f47f8370126bf11b7f0a3df03b19ad6ac49
Michael Leigeber <michael@caddis.co>
2015-08-14 10:55:12 -0500

    Rename the clean command to reset

---
+++ commands/reset.js
@@ -0,0 +1,23 @@
+(function() {
+	'use strict';
+
+	module.exports = function(config) {
+		var fs = require('fs-extra'),
+			path = require('path'),
+			files = [
+				'bower.json',
+				'LICENSE',
+				'README.md',
+				'source/modules/welcome'
+			];
+
+		files.forEach(function(file) {
+			fs.remove(path.join(config.rootPath, file));
+		});
+
+		Wee.notify({
+			title: 'Project Reset',
+			message: 'Sample project files have been removed'
+		});
+	};
+})();
