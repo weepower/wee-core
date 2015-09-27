86b3d48eed9774bbedd8471a3b3570ee550ddfc6
Michael Leigeber <michael@leigeber.com>
2015-09-27 15:41:42 -0500

    Add pass-through run command for calling Grunt tasks

---
+++ commands/run.js
@@ -0,0 +1,14 @@
+(function() {
+	'use strict';
+
+	module.exports = function(config) {
+		var cp = require('child_process'),
+			task = 'default';
+
+		if (config.options.length) {
+			task = config.options[0];
+		}
+
+		cp.execSync('grunt ' + task, {stdio: [0,1,2]});
+	};
+})();
