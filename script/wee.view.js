495613c4049a011e3da4271bc90d6c65bd43c887
Michael Leigeber <michael@leigeber.com>
2015-10-06 08:17:27 -0500

    Add support for {{ else }} tag Consolidate filter functionality into helpers Allow helpers to return modified values

--- script/wee.view.js
+++ script/wee.view.js
@@ -4,7 +4,7 @@
 	'use strict';
 
 	var reg = {
-			tags: /{{\s*([#\/])([^#{\|\n]+)(\|[^{\n]+)?}}/g,
+			tags: /{{\s*(?:([#\/])([^#{\|\n\s]+)\s*(\|[^{\n]+)?|else)\s*}}/g,
 			partial: /{{\s*> (.+?)}}/g,
 			pair: /{{#(.+?)(?:|\|?([^}]*))}}([\s\S]*?){{\/\1}}/g,
 			single: /{{(.+?)}}/g,
@@ -13,12 +13,12 @@
 			args: /(\\?['"][^'"]+\\?['"]|[^,]+)/g
 		},
 		_isEmpty = function(val) {
-			return val == '' ||
+			return val === '' ||
 				val === false ||
 				val == null ||
 				(typeof val == 'object' && ! Object.keys(val).length);
 		},
-		filters = {
+		helpers = {
 			is: function(val) {
 				return this.val === val;
 			},
@@ -32,7 +32,6 @@
 				return ! this.empty;
 			}
 		},
-		helpers = {},
 		partials = {},
 		esc;
 
@@ -75,23 +74,13 @@
 		},
 
 		/**
-		 * Add conditional template handler or data modifier
-		 *
-		 * @param {string} name
-		 * @param {function} fn
-		 */
-		addFilter: function(name, fn) {
-			this.$private.extend(filters, name, fn);
-		},
-
-		/**
-		 * Add helper to run additional processing on tag data
+		 * Add conditional template handler or tag data modifier
 		 *
 		 * @param {string} name
 		 * @param {function} fn
 		 */
 		addHelper: function(name, fn) {
-			this.$private.extend(helpers, name, fn);
+			W._extend(helpers, name, fn);
 		},
 
 		/**
@@ -101,27 +90,9 @@
 		 * @param {string} value
 		 */
 		addPartial: function(name, value) {
-			this.$private.extend(partials, name, value);
+			W._extend(partials, name, value);
 		}
 	}, {
-		/**
-		 * Extend view engine
-		 *
-		 * @param {string} type
-		 * @param {(object|string)} a
-		 * @param {function} b
-		 */
-		extend: function(type, a, b) {
-			var obj = a;
-
-			if (typeof a == 'string') {
-				obj = [];
-				obj[a] = b;
-			}
-
-			W.$extend(type, obj);
-		},
-
 		/**
 		 * Render template string
 		 *
@@ -130,7 +101,8 @@
 		 * @returns {string}
 		 */
 		render: function(temp, data) {
-			var tags = [];
+			var tags = [],
+				depth = [];
 
 			// Make partial replacements and match tag pairs
 			temp = temp.replace(reg.partial, function(match, tag) {
@@ -141,32 +113,39 @@
 						partial() :
 						partial
 				) : '';
-			}).replace(reg.tags, function(m, pre, tag, filter) {
-				tag = tag.trim();
+			}).replace(reg.tags, function(m, pre, tag, helper) {
+				var resp = '{{';
 
-				var segs = tag.split('('),
-					root = segs[0],
-					resp = '{{' + pre,
-					exists = tags.hasOwnProperty(tag);
+				if (pre) {
+					var segs = tag.split('('),
+						root = segs[0],
+						exists = tags.hasOwnProperty(tag);
+					resp += pre + root + '%';
 
-				if (pre == '#' && tag) {
-					if (exists) {
-						tags[root].i++;
-						tags[root].o.push(tags[root].i);
-					} else {
-						tags[root] = {
-							i: 1,
-							o: [1]
-						};
+					if (pre == '#') {
+						depth.push(root);
+
+						if (exists) {
+							tags[root].i++;
+							tags[root].o.push(tags[root].i);
+						} else {
+							tags[root] = {
+								i: 1,
+								o: [1]
+							};
+						}
+
+						if (segs.length > 1) {
+							helper = '(' + segs.slice(1).join('(');
+						}
+
+						resp += tags[root].i + (helper || '');
+					} else if (exists) {
+						resp += tags[root].o.pop();
 					}
-
-					if (segs.length > 1) {
-						filter = '(' + segs.slice(1).join('(');
-					}
-
-					resp += root + '%' + tags[root].i + (filter || '');
-				} else if (exists) {
-					resp += root + '%' + tags[root].o.pop();
+				} else {
+					tag = depth.pop();
+					resp += ':' + tag + '%' + tags[tag].i;
 				}
 
 				return resp + '}}';
@@ -196,31 +175,34 @@
 		parse: function(temp, data, prev, init, index) {
 			var scope = this;
 
-			return temp.replace(reg.pair, function(m, tag, filter, inner) {
-				// Remove tag depth suffix
-				tag = tag.replace(/%\d+/, '');
+			return temp.replace(reg.pair, function(m, t, helper, inner) {
+				var tag = t.replace(/%\d+/, '');
 
 				// Escape child template tags
 				if (tag == '!') {
 					esc = true;
 
-					return inner.replace(/{{/g, '{~')
-						.replace(/}}/g, '~}');
+					return inner.replace(/{{:[^{\s]+}}/g, '{{ else }}')
+						.replace(/{{/g, '{~ ')
+						.replace(/}}/g, ' ~}');
 				}
 
+				var cond = inner.split('{{:' + t + '}}');
+				inner = cond[0];
 				esc = false;
 
 				var val = scope.get(data, prev, tag, U, init, index),
 					empty = _isEmpty(val),
-					filt = [],
+					help = [],
 					each;
 
-				// Parse filters
-				if (filter || empty) {
-					var meth = filter ? filter.split('|') : [],
+				// Parse helpers
+				if (helper || empty) {
+					var meth = helper ? helper.split('|') : [],
+						sec = cond.length > 1 ? cond[1] : '',
 						agg = [];
 
-					// Check for root filters
+					// Check for root helpers
 					if (empty) {
 						var arg = meth[0] && meth[0][0] == '(';
 						meth.unshift(tag + (arg ? meth[0] : '()'));
@@ -230,34 +212,47 @@
 						}
 					}
 
-					// Capture available aggregates and filters
+					// Capture available aggregates and helpers
 					meth.forEach(function(el) {
 						var arr = el.match(reg.ext),
 							name = arr[1].trim();
 
-						// Check for each filter
+						// Check for each helper
 						if (name == 'each') {
 							each = true;
-						} else if (filters[name]) {
-							el = [filters[name], arr[2]];
-							each ? filt.push(el) : agg.push(el);
+						} else if (helpers[name]) {
+							el = [helpers[name], arr[2]];
+							each ? help.push(el) : agg.push(el);
 						}
 					});
 
 					// Process aggregates
 					if (agg.length) {
 						if (! agg.every(function(f) {
-								return f[0].apply({
+								var rv = f[0].apply({
 									val: val,
 									data: data,
 									root: init,
 									tag: tag,
 									empty: empty,
 									index: index
-								}, scope.parseArgs(f[1], val)) !== false;
+								}, scope.parseArgs(f[1], val));
+
+								if (rv === false) {
+									return rv;
+								}
+
+								if (rv !== true) {
+									val = rv;
+									empty = _isEmpty(val);
+								}
+
+								return true;
 							})) {
-							return '';
+							return sec;
 						}
+					} else if (empty) {
+						return sec;
 					}
 				}
 
@@ -285,8 +280,32 @@
 						for (var key in val) {
 							if (val.hasOwnProperty(key)) {
 								// Merge default properties
-								var el = val[key],
-									item = W.$extend({}, W.$isObject(el) ?
+								var el = val[key];
+								empty = _isEmpty(el);
+
+								// Process helpers
+								if (! help.length ||
+									help.every(function(f) {
+										var rv = f[0].apply({
+											val: el,
+											data: data,
+											root: init,
+											tag: tag,
+											empty: empty,
+											index: index
+										}, scope.parseArgs(f[1], val));
+
+										if (rv === false) {
+											return rv;
+										}
+
+										if (rv !== true) {
+											el = rv;
+										}
+
+										return true;
+									})) {
+									var item = W.$extend({}, W.$isObject(el) ?
 										el :
 										(isPlainObject ? val : {}),
 									{
@@ -296,24 +315,10 @@
 										'##': i + 1
 									});
 
-								empty = _isEmpty(el);
+									i++;
 
-								// Process filters
-								if (! filt.length ||
-									filt.every(function(f) {
-										return f[0].apply({
-											val: el,
-											data: val,
-											root: init,
-											tag: tag,
-											empty: empty,
-											index: i
-										}, scope.parseArgs(f[1], el)) !== false;
-									})) {
 									resp += scope.parse(inner, item, data, init, i);
 								}
-
-								i++;
 							}
 						}
 
@@ -328,7 +333,7 @@
 					segs = split[0].split('|'),
 					tag = segs[0].trim(),
 					val = scope.get(data, prev, tag, fb, init, index),
-					help = segs.length > 1 ? segs.slice(1) : segs;
+					helps = segs.length > 1 ? segs.slice(1) : segs;
 
 				// Return empty string if output isn't available
 				if (val === U || typeof val == 'object') {
@@ -336,9 +341,9 @@
 				}
 
 				// Process helpers
-				help.forEach(function(el, i) {
+				helps.forEach(function(el, i) {
 					var arr = el.match(reg.ext);
-					help[i] = el.trim();
+					helps[i] = el.trim();
 
 					if (arr) {
 						var helper = helpers[arr[1].trim()];
@@ -360,7 +365,7 @@
 				if (val === U) {
 					val = '';
 				} else if (typeof val == 'string') {
-					if (help.indexOf('raw') < 0) {
+					if (helps.indexOf('raw') < 0) {
 						val = val.replace(/&amp;/g, '&')
 							.replace(/&/g, '&amp;')
 							.replace(/</g, '&lt;')
@@ -438,7 +443,7 @@
 		},
 
 		/**
-		 * Parse filter and helper arguments
+		 * Parse helper arguments
 		 *
 		 * @param {string} str
 		 * @returns {*}
