816d025a740bc919bba17a9ee5e8377a1981a603
Michael Leigeber <michael@leigeber.com>
2015-10-24 13:13:21 -0500

    Add pass-through command line argument support to the run command

--- commands/run.js
+++ commands/run.js
@@ -5,14 +5,19 @@
 
 	module.exports = function(config) {
 		var cp = require('child_process'),
-			task = 'default';
+			task = 'default',
+			args = '';
 
 		if (config.options.length) {
 			task = config.options[0];
 		}
 
+		Object.keys(config.args).forEach(function(key) {
+			args += ' --' + key + '=' + config.args[key];
+		});
+
 		cp.execSync('grunt ' + task + ' --b ' + __dirname +
-			'/../ --gruntfile ' + __dirname + '/../Gruntfile.js', {
+			'/../ --gruntfile ' + __dirname + '/../Gruntfile.js' + args, {
 			stdio: [0, 1, 2]
 		});
 	};
