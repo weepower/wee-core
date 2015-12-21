3260a328e2c234e0b84896a0653b0dbac4e00878
Michael Leigeber <michael@leigeber.com>
2015-12-20 21:17:44 -0600

    Make minor ordering and comment updates

--- build/tasks/core-cleanup.js
+++ build/tasks/core-cleanup.js
@@ -2,10 +2,10 @@
 
 module.exports = function(grunt) {
 	grunt.registerTask('cleanup', function() {
-		// Remove temporary files
+		// Remove temporary core files
 		fs.emptyDirSync(config.paths.temp);
 
-		// Remove all public assets
+		// Remove compiled public assets
 		fs.removeSync(config.paths.css);
 		fs.removeSync(config.paths.img);
 		fs.removeSync(config.paths.js);
