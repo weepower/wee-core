df8210fcb367902cb983d6f3d8c48b5fe1c1d4fc
Michael Leigeber <michael@leigeber.com>
2016-03-01 08:43:02 -0600

    Implement proper mobile touch events Add movement implementation to limit opposing scrolling when firing the end event

--- js/wee.touch.js
+++ js/wee.touch.js
@@ -2,35 +2,42 @@
 	'use strict';
 
 	var events = [
-			['swipeLeft', 'X', 1],
-			['swipeRight', 'X', -1],
-			['swipeUp', 'Y', 1],
-			['swipeDown', 'Y', -1]
-		],
-		down = 'mousedown',
-		up = 'mouseup';
+		['swipeLeft', 'X', 'Y', 1],
+		['swipeRight', 'X', 'Y',-1],
+		['swipeUp', 'Y', 'X', 1],
+		['swipeDown', 'Y','X', -1]
+	];
 
 	/**
 	 * Loop through custom events and bind applicable standard events
 	 */
 	events.forEach(function(event) {
-		E.addEvent(event[0], function(el, fn, conf) {
-			var scope = this,
-				distance = conf.distance || 50;
+		var ns = event[0],
+			start = 'mousedown.' + ns + ' touchstart.' + ns,
+			end = 'mouseup.' + ns + ' touchend.' + ns;
 
-			E.on(el, down + '.' + event[0], function(e) {
-				scope.start = e['client' + event[1]];
+		E.addEvent(ns, function(el, fn, conf) {
+			var scope = this,
+				distance = conf.distance || 50,
+				movement = conf.movement || 25;
+
+			E.on(el, start, function(e) {
+				var a = e.touches ? e.touches[0] : e;
+				scope.start = a['page' + event[1]];
+				scope.scroll = W._win['page' + event[2] + 'Offset'];
 			}, conf);
 
-			E.on(el, up + '.' + event[0], function(e, el) {
-				var travel = (scope.start - e['client' + event[1]]) * event[2];
+			E.on(el, end, function(e, el) {
+				var a = e.changedTouches ? e.changedTouches[0] : e,
+					travel = (scope.start - a['page' + event[1]]) * event[3],
+					scroll = Math.abs(scope.scroll - W._win['page' + event[2] + 'Offset']);
 
-				if (travel > distance) {
+				if (travel > distance && scroll < movement) {
 					fn.apply(conf.scope || el, W._slice.call(arguments));
 				}
 			}, conf);
 		}, function(el, fn) {
-			E.off(el, down + '.' + event[0] + ' ' + up + '.' + event[0], fn);
+			E.off(el, start + ' ' + end, fn);
 		});
 	});
 })(Wee, Wee.events);
