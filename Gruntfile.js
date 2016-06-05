07ecffa619ceed03badc491cbf646156cf97eb4a
Michael Leigeber <michael@leigeber.com>
2016-06-05 10:31:00 -0500

    Update version to match upcoming framework release

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@
 		var LessCssClean = require('less-plugin-clean-css'),
 			rootPath = '../../';
 
-		global.version = '3.3.2';
+		global.version = '3.3.4';
 
 		global.bs = require('browser-sync').create();
 		global.fs = require('fs-extra');
