c7ce68ff26bd66a2faabd5f7c397d17cd86100c5
Peter Bedor Jr <peter@caddis.co>
2016-10-30 16:21:01 -0500

    Add new tests for js and css assets loading

--- js/tests/unit/wee.assets.js
+++ js/tests/unit/wee.assets.js
@@ -35,22 +35,86 @@ define(function(require) {
 		},
 
 		load: {
-			'single file': function() {
-				var promise = this.async(1000);
+			'single file': {
+				file: function() {
+					var promise = this.async(1000);
 
-				Wee.assets.load({
-					root: '/js/tests/support/sample-files/',
-					files: 'sample.js',
-					success: promise.callback(function() {
-						assert.isTrue(Wee.$get('sampleJsLoaded'),
-							'Sample file not loaded successfully'
-						);
+					Wee.assets.load({
+						root: '/js/tests/support/sample-files/',
+						files: 'sample.js',
+						success: promise.callback(function() {
+							assert.isTrue(Wee.$get('sampleJsLoaded'),
+								'Sample file not loaded successfully'
+							);
 
-						Wee.assets.remove('/js/' +
-							'tests/sample-files/sample.js'
-						);
-					})
-				});
+							Wee.assets.remove(
+								'/js/tests/support/sample-files/sample.js'
+							);
+						})
+					});
+				},
+
+				js: function() {
+					var promise = this.async(1000);
+
+					Wee.assets.load({
+						root: '/js/tests/support/sample-files/',
+						js: 'sample.js',
+						success: promise.callback(function() {
+							assert.isTrue(Wee.$get('sampleJsLoaded'),
+								'Sample file not loaded successfully'
+							);
+
+							Wee.assets.remove(
+								'/js/tests/support/sample-files/sample.js'
+							);
+						})
+					});
+				},
+
+				css: function() {
+					var promise = this.async(1000);
+
+					var container = document.createElement('div');
+
+					container.className = 'container';
+
+					document.body.appendChild(container);
+
+					Wee.assets.load({
+						root: '/js/tests/support/sample-files/',
+						css: 'sample.css',
+						success: promise.callback(function() {
+							assert.equal(Wee.$css('.container', 'backgroundColor'),
+								'rgb(255, 0, 0)',
+								'Sample file not loaded successfully'
+							);
+
+							Wee.$remove('.container');
+
+							Wee.assets.remove(
+								'/js/tests/support/sample-files/sample.css'
+							);
+						})
+					});
+				},
+
+				// img: function() {
+
+				// TODO: find out how to test this or what it even does
+				// 	var promise = this.async(1000);
+
+				// 	Wee.assets.load({
+				// 		root: '/js/tests/support/sample-files/',
+				// 		img: 'logo.png',
+				// 		success: promise.callback(function() {
+
+				// 			Wee.assets.remove(
+				// 				'/js/tests/support/sample-files/logo.png'
+				// 			);
+				// 		})
+				// 	})
+				// }
 			},
 
 			'multiple files': function() {
