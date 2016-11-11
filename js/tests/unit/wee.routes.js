4a652ea9b0b4b6425272cc0250322031de99b743
Peter Bedor Jr <peter@caddis.co>
2016-11-10 23:57:57 -0600

    Add additional coverage for routes

--- js/tests/unit/wee.routes.js
+++ js/tests/unit/wee.routes.js
@@ -7,6 +7,11 @@ define(function(require) {
 	registerSuite({
 		name: 'Routes',
 
+		afterEach: function() {
+			$.set('test', false);
+			$.set('test2', false);
+		},
+
 		uri: {
 			get: function() {
 				assert.isObject(Wee.routes.uri(),
@@ -86,35 +91,48 @@ define(function(require) {
 
 		map: {
 			set: function() {
-				Wee.routes.map({
+				var routes = Wee.routes.map({
 				    '$any': 'common',
 				    'script': {
 				        'routes': function() {
-
+							return 'test';
 				        }
 				    }
 				});
 
 				assert.isObject(Wee.routes.map());
+
+				assert.strictEqual(routes.script.routes(), 'test',
+					'Route function was not executed successfully'
+				);
 			}
 		},
 
 		run: {
 			simple: function() {
-				// TODO: Complete
 				Wee.routes.map({
 					'$any': function() {
-						return 'any';
+						$.set('test', true);
+					},
+					'$root': function() {
+						$.set('test2', true);
 					}
 				});
 
-				assert.strictEqual(Wee.routes.run(), undefined,
-					'Any route executed successfully.'
+				Wee.routes.run({
+					path: '/'
+				});
+
+				assert.isTrue($.get('test'),
+					'$any was not evaulated correctly'
+				);
+
+				assert.isTrue($.get('test2'),
+					'$root was not evaulated correctly'
 				);
 			},
 
 			advanced: function() {
-				// TODO: Complete
 				Wee.routes.run({
 					path: '/test/route',
 					routes: {
@@ -127,7 +145,91 @@ define(function(require) {
 						}
 					}
 				});
+
+				Wee.routes.map({
+					'$root': function() {
+						$.set('test', true);
+					}
+				}, true);
+
+				assert.isTrue($.get('test'),
+					'Routes were not initialized'
+				);
 			}
-		}
+		},
+
+		addFilter: function() {
+			Wee.routes.addFilter('isInternal', function(seg) {
+				return ['admin', 'protected'].indexOf(seg) > -1;
+			});
+
+			Wee.fn.make('testController', {
+				init: function() {
+					$.set('test', true);
+				}
+			});
+
+			Wee.routes.map({
+				'$isInternal:eval': 'testController'
+			});
+
+			Wee.routes.run({
+				path: '/admin'
+			});
+
+			assert.isTrue($.get('test'),
+				'Route filter was not executed correctly'
+			);
+		},
+
+		once: function() {
+			var i = 0;
+
+			Wee.routes.map({
+				'$any:once': function() {
+					i++;
+				}
+			});
+
+			Wee.routes.run({
+				path: '/'
+			});
+
+			Wee.routes.run({
+				path: '/'
+			});
+
+			assert.strictEqual(i, 1,
+				'Route was executed twice despite being instructed to execute once'
+			);
+		},
+
+		controller: function() {
+			Wee.fn.make('testController', {
+				init: function() {
+					$.set('test', true);
+				},
+				test: function() {
+					$.set('test2', true);
+				}
+			});
+
+			Wee.routes.map({
+				'$any': [
+					'testController:init',
+					'testController:test'
+				]
+			});
+
+			Wee.routes.run();
+
+			assert.strictEqual($.get('test'), true,
+				'Init method was not executed successfully'
+			);
+
+			assert.strictEqual($.get('test2'), true,
+				'Method was not executed successfully'
+			);
+		},
 	});
 });
