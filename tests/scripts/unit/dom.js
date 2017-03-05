5f5030a51766e2514ef4e7dd5cf109fd2a9568dc
Nathan Hood <nathanhood@me.com>
2017-03-05 09:02:01 -0600

    Update test case for testing karma/webpack

--- tests/scripts/unit/dom.js
+++ tests/scripts/unit/dom.js
@@ -1,10 +1,7 @@
-var expect = require('chai').expect;
+import { getSpecialValue } from 'test';
 
-describe('webdriver.io page', function() {
-	it('should have the right title - the fancy generator way', function () {
-		browser.url('http://webdriver.io');
-		var title = browser.getTitle();
-
-		expect(title).to.equal('WebdriverIO - Selenium 2.0 javascript bindings for nodejs');
+describe('Test', function() {
+	it('should do what I want it to', function () {
+		expect(getSpecialValue()).to.equal(10);
 	});
 });
