cdef9490c836b444c006124d5970cc84bd8afb04
Nathan Hood <nathanhood@me.com>
2017-03-05 21:16:57 -0600

    Update karma reporting

--- tests/scripts/karma.conf.js
+++ tests/scripts/karma.conf.js
@@ -32,6 +32,9 @@ module.exports = function(config) {
 		// possible values: 'dots', 'progress'
 		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
 		reporters: ['mocha'],
+		mochaReporter: {
+			showDiff: true
+		},
 
 		// web server port
 		port: 9876,
@@ -41,7 +44,7 @@ module.exports = function(config) {
 
 		// level of logging
 		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-		logLevel: config.LOG_INFO,
+		logLevel: config.LOG_ERROR,
 
 		// enable / disable watching file and executing tests whenever any file changes
 		autoWatch: true,
