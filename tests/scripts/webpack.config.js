89c22501050698fde10f1abf0c9ea9f3fbe44476
Nathan Hood <nathanhood@me.com>
2017-03-05 21:14:37 -0600

    Update path resolution for scripts

--- tests/scripts/webpack.config.js
+++ tests/scripts/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
 	resolve: {
 		modules: [
-			__dirname + '/scripts',
+			__dirname + '/../../scripts',
 			path.resolve(__dirname, "./node_modules")
 		]
 	}
