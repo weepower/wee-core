6986a2166823e2f4248deebec7ae849416904a9c
Nathan Hood <nathanhood@me.com>
2017-03-05 21:17:31 -0600

    Update basePath variable
    __dirname was not adding value here

--- tests/scripts/karma.conf.js
+++ tests/scripts/karma.conf.js
@@ -1,9 +1,7 @@
-const root = __dirname;
-
 module.exports = function(config) {
 	config.set({
 		// base path that will be used to resolve all patterns (eg. files, exclude)
-		basePath: root,
+		basePath: '',
 
 		// frameworks to use
 		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
