55364c26a69a5bd403ed43fbbd6b483407ef7fbf
Nathan Hood <nathanhood@me.com>
2017-03-12 20:37:11 -0500

    Add $, $each, and $setRef

--- scripts/core/dom.js
+++ scripts/core/dom.js
@@ -1,7 +1,178 @@
-import { _doc, _body } from 'core/variables';
+import { _body, _doc, _html, _slice, _win, U } from 'core/variables';
 import { $exec } from 'core/core';
+import { _extend, $toArray } from 'core/types';
 
-let range;
+let range,
+	refs = {};
+
+/**
+ * Check if a node contains another node
+ *
+ * @private
+ * @param {HTMLElement} source
+ * @param {HTMLElement} target
+ * @returns {boolean} match
+ */
+const _contains = (source, target) => {
+	return (source === _doc ? _html : source)
+		.contains(target);
+};
+
+/**
+ * Convert selection to array
+ *
+ * @protected
+ * @param {($|HTMLElement|string)} selector
+ * @param {object} [options]
+ * @param {(HTMLElement|string)} [options.context=document]
+ * @returns {($|Array)} nodes
+ */
+export const _selArray = (selector, options) => {
+	if (selector && selector._$) {
+		return selector;
+	}
+
+	options = options || {};
+
+	let el = typeof selector == 'string' ?
+		$(selector, options.context) :
+		selector;
+
+	return $toArray(el);
+};
+
+/**
+ * Get matches to specified selector or return parsed HTML
+ *
+ * @param {($|HTMLElement|string)} selector
+ * @param {($|HTMLElement|string)} [context=document]
+ * @returns {Array} elements
+ */
+export const $ = (selector, context) => {
+	let el = null,
+		ref = [];
+
+	if (typeof selector != 'string') {
+		el = selector;
+	} else {
+		if (selector == 'window') {
+			return [_win];
+		}
+
+		if (selector == 'document') {
+			return [_doc];
+		}
+
+		// Return nothing if context doesn't exist
+		context = context !== U ? $(context)[0] : _doc;
+
+		if (! context) {
+			return ref;
+		}
+
+		// Check for pre-cached elements
+		if (selector.indexOf('ref:') > -1) {
+			let split = selector.split(',').filter(function(sel) {
+				sel = sel.trim();
+
+				if (sel.slice(0, 4) == 'ref:') {
+					sel = sel.slice(4);
+					sel = refs[sel];
+
+					// Apply context filter if not document
+					if (sel) {
+						ref = ref.concat(
+							context === _doc ?
+								sel :
+								sel.filter(function(el) {
+									return _contains(context, el);
+								})
+						);
+					}
+
+					return false;
+				}
+
+				return true;
+			});
+
+			if (split.length) {
+				selector = split.join(',');
+			} else {
+				return ref;
+			}
+		}
+
+		// Use third-party selector engine if defined
+		if (_win.WeeSelector !== U) {
+			el = _win.WeeSelector(selector, context);
+		} else if (/^[#.]?[\w-]+$/.test(selector)) {
+			let pre = selector[0];
+
+			if (pre == '#') {
+				el = _doc.getElementById(selector.substr(1));
+			} else if (pre == '.') {
+				el = context.getElementsByClassName(selector.substr(1));
+			} else {
+				el = context.getElementsByTagName(selector);
+			}
+		} else {
+			try {
+				el = context.querySelectorAll(selector);
+			} catch (e) {
+				el = $parseHTML(selector).childNodes;
+			}
+		}
+	}
+
+	if (! el) {
+		el = ref;
+	} else if (el.nodeType !== U || el === _win) {
+		el = [el];
+	} else {
+		el = _slice.call(el);
+	}
+
+	// Join references if available
+	return ref.length ? el.concat(ref) : el;
+};
+
+/**
+ * Execute function for each matching selection
+ *
+ * @param {($|Array|HTMLElement|string)} target
+ * @param {function} fn
+ * @param {object} [options]
+ * @param {Array} [options.args]
+ * @param {($|HTMLElement|string)} [options.context=document]
+ * @param {boolean} [options.reverse=false]
+ * @param {object} [options.scope]
+ */
+export const $each = (target, fn, options) => {
+	if (target) {
+		let conf = _extend({
+				args: []
+			}, options),
+			els = _selArray(target, conf),
+			i = 0;
+
+		if (conf.reverse && ! els._$) {
+			els = els.reverse();
+		}
+
+		for (; i < els.length; i++) {
+			let el = els[i],
+				val = $exec(fn, {
+					args: [el, i].concat(conf.args),
+					scope: conf.scope || el
+				});
+
+			if (val === false) {
+				return;
+			}
+		}
+	}
+};
 
 /**
  * Create document fragment from an HTML string
@@ -33,6 +204,36 @@ export const $parseHTML = html => {
 	}
 
 	return el;
+};
+
+/**
+ * Add ref elements to datastore
+ *
+ * @param {(HTMLElement|string)} [context=document]
+ */
+export const $setRef = context => {
+	context = context ? $(context)[0] : _doc;
+
+	// Clear existing refs if reset
+	Object.keys(refs).forEach(function(val) {
+		refs[val] = refs[val].filter(function(el) {
+			return ! (
+				! _contains(_doc, el) ||
+				(_contains(context, el) && context !== el)
+			);
+		});
+	});
+
+	// Set refs from DOM
+	$each('[data-ref]', function(el) {
+		el.getAttribute('data-ref').split(/\s+/)
+			.forEach(function(val) {
+				refs[val] = refs[val] || [];
+				refs[val].push(el);
+			});
+	}, {
+		context: context
+	});
 };
 
 /**
--- tests/scripts/unit/core/dom.js
+++ tests/scripts/unit/core/dom.js
@@ -1,8 +1,175 @@
-import { ready, $parseHTML } from 'core/dom';
+import { _doc, _win } from 'core/variables';
+import { ready, $each, $parseHTML, $, $setRef } from 'core/dom';
 
 describe('Core: DOM', () => {
-	beforeEach(() => {
-		document.querySelector('body').innerHTML = '';
+	describe('$', () => {
+		before(() => {
+			let div = document.createElement('div');
+
+			div.textContent = 'test';
+			div.classList = 'test';
+			div.setAttribute('data-ref', 'testRef');
+			document.querySelector('body').appendChild(div);
+		});
+
+		after(() => {
+			document.querySelector('body').innerHTML = '';
+		});
+
+		it('should return cached response when selecting window', () => {
+			expect($('window')[0]).to.equal(_win);
+		});
+
+		it('should return cached response when selecting document', () => {
+			expect($('document')[0]).to.equal(_doc);
+		});
+
+		it('should select elements by passing dom node', () => {
+			let span = document.createElement('span');
+			document.querySelector('body').appendChild(span);
+
+			expect($(span)[0]).to.equal(span);
+		});
+
+		it('should select elements by element name', () => {
+			expect($('div')[0].textContent).to.equal('test');
+		});
+
+		it('should select elements by class name', () => {
+			expect($('.test')[0].textContent).to.equal('test');
+		});
+
+		it('should select elements with context', () => {
+			expect($('.test', 'body')[0].textContent).to.equal('test');
+			expect($('.test', 'div')[0]).to.be.undefined;
+		});
+
+		it('should select elements by data ref', () => {
+			$setRef();
+			expect($('ref:testRef')[0].textContent).to.equal('test');
+		});
+	});
+
+	describe('$each', () => {
+		before(() => {
+			let html = `<div>
+					<div class="each">1</div>
+					<div class="each">2</div>
+					<div class="each">3</div>
+					<div class="context">
+						<div class="each">4</div>
+					</div>
+					</div>`,
+				fragment = document.createRange().createContextualFragment(html);
+
+			document.querySelector('body').appendChild(fragment);
+		});
+
+		after(() => {
+			document.querySelector('body').innerHTML = '';
+		});
+
+		it('should execute function for each matching selection', () => {
+			let count = 0;
+
+			$each($('.each'), el => {
+				count = el.textContent;
+			});
+
+			expect(count).to.equal('4');
+		});
+
+		it('should iterate in reverse', () => {
+			let count = 0;
+
+			$each($('.each'), el => {
+				count = el.textContent;
+			}, {
+				reverse: true
+			});
+
+			expect(count).to.equal('1');
+		});
+
+		it('should inject scope into callback functions', () => {
+			let scope = 0;
+
+			$each($('.each'), function(el) {
+				scope = this.a;
+			}, {
+				scope: {a: 1}
+			});
+
+			expect(scope).to.equal(1);
+		});
+
+		it('should filter based on context', () => {
+			let count;
+
+			$each('.each', function(el, i) {
+				count = i;
+			}, {
+				context: '.context'
+			});
+
+			expect(count).to.equal(0);
+		});
+	});
+
+	describe('$parseHTML', () => {
+		afterEach(() => {
+			document.querySelector('body').innerHTML = '';
+		});
+
+		it('should create a document fragment from HTML string', () => {
+			let html = '<div class="container"><ul><li class="child">1</li><li class="child">2</li></ul></div>',
+				fragment = $parseHTML(html);
+
+			// Fragments should be have DOM api accessible, even before on document
+			expect(fragment.querySelector('.child').textContent).to.equal('1');
+
+			document.querySelector('body').appendChild(fragment);
+
+			expect(document.querySelector('.container').innerHTML).to.equal('<ul><li class="child">1</li><li class="child">2</li></ul>');
+		});
+	});
+
+	describe('$setRef', () => {
+		beforeEach(() => {
+			let div = document.createElement('div');
+
+			// Reset registry
+			$setRef();
+
+			// Define base element
+			div.setAttribute('data-ref', 'testRef');
+			document.querySelector('body').appendChild(div);
+		});
+
+		afterEach(() => {
+			// Reset DOM
+			document.querySelector('body').innerHTML = '';
+		});
+
+		it('should register elements with data-ref attribute', () => {
+			expect($('ref:testRef').length).to.equal(0);
+			$setRef();
+			expect($('ref:testRef').length).to.equal(1);
+		});
+
+		it('should register elements within provided context', () => {
+			let html = `<div>
+					<div class="context">
+						<div data-ref="testRef"></div>
+					</div>
+				</div>`,
+				fragment = document.createRange().createContextualFragment(html);
+
+			document.querySelector('body').appendChild(fragment);
+			$setRef('.context');
+
+			expect($('ref:testRef').length).to.equal(1);
+		});
 	});
 
 	describe('ready', () => {
@@ -14,20 +181,6 @@ describe('Core: DOM', () => {
 				expect(state).to.be.true;
 				done();
 			});
-		});
-	});
-
-	describe('$parseHTML', () => {
-		it('should create a document fragment from HTML string', () => {
-			let html = '<div class="container"><ul><li class="child">1</li><li class="child">2</li></ul></div>',
-				fragment = $parseHTML(html);
-
-			// Fragments should be have DOM api accessible, even before on document
-			expect(fragment.querySelector('.child').textContent).to.equal('1');
-
-			document.querySelector('body').appendChild(fragment);
-
-			expect(document.querySelector('.container').innerHTML).to.equal('<ul><li class="child">1</li><li class="child">2</li></ul>');
 		});
 	});
 });
