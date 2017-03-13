a3bd2b7fb484821ceb1ff16af3ad771a02466dbf
Nathan Hood <nathanhood@me.com>
2017-03-12 19:12:25 -0500

    Add $parseHTML

--- scripts/core/dom.js
+++ scripts/core/dom.js
@@ -1,5 +1,39 @@
-import { _doc } from 'core/variables';
+import { _doc, _body } from 'core/variables';
 import { $exec } from 'core/core';
+
+let range;
+
+/**
+ * Create document fragment from an HTML string
+ *
+ * @param {string} html
+ * @returns {HTMLElement} element
+ */
+export const $parseHTML = html => {
+	let el;
+	html = html.trim();
+
+	if (! range) {
+		range = _doc.createRange();
+		range.selectNode(_body);
+	}
+
+	if (range && range.createContextualFragment) {
+		el = range.createContextualFragment(html);
+	} else {
+		let div = _doc.createElement('div'),
+			child;
+		el = _doc.createDocumentFragment();
+
+		div.innerHTML = html;
+
+		while (child = div.firstChild) {
+			el.appendChild(child);
+		}
+	}
+
+	return el;
+};
 
 /**
  * Execute specified function when document is ready
--- tests/scripts/unit/core/dom.js
+++ tests/scripts/unit/core/dom.js
@@ -1,6 +1,10 @@
-import { ready } from 'core/dom';
+import { ready, $parseHTML } from 'core/dom';
 
 describe('Core: DOM', () => {
+	beforeEach(() => {
+		document.querySelector('body').innerHTML = '';
+	});
+
 	describe('ready', () => {
 		it('should execute callback when document has finished loading', done => {
 			let state = false;
@@ -10,6 +14,20 @@ describe('Core: DOM', () => {
 				expect(state).to.be.true;
 				done();
 			});
+		});
+	});
+
+	describe('$parseHTML', () => {
+		it('should create a document fragment from HTML string', () => {
+			let html = '<div class="container"><ul><li class="child">1</li><li class="child">2</li></ul></div>',
+				fragment = $parseHTML(html);
+
+			// Fragments should be have DOM api accessible, even before on document
+			expect(fragment.querySelector('.child').textContent).to.equal('1');
+
+			document.querySelector('body').appendChild(fragment);
+
+			expect(document.querySelector('.container').innerHTML).to.equal('<ul><li class="child">1</li><li class="child">2</li></ul>');
 		});
 	});
 });
