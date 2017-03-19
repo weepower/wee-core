50ad8ea582c2ec1a48a6c73687deb174cde8a02b
Nathan Hood <nathanhood@me.com>
2017-03-19 01:20:03 -0500

    Try firefox with travis

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -68,7 +68,7 @@ module.exports = function(config) {
 				flags: ['--no-sandbox']
 			}
 		},
-		browsers: env === 'ci' ? ['Chrome_travis_ci'] : ['Chrome'],
+		browsers: env === 'ci' ? ['Firefox'] : ['Chrome'],
 
 		// Continuous Integration mode
 		// if true, Karma captures browsers, runs the tests and exits
