2b7152566f2ebea0e61ac10e4ab19339cd984c71
Nathan Hood <nathanhood@me.com>
2017-04-12 19:58:52 -0500

    Add notify and logError methods

---
+++ utils.js
@@ -0,0 +1,48 @@
+var chalk = require('chalk'),
+	fs = require('fs-extra'),
+	notifier = require('node-notifier'),
+	path = require('path');
+
+module.exports = {
+	/**
+	 * Notify through a natively available notification option
+	 *
+	 * @param {object} options
+	 * @param {string} [type=notice]
+	 * @param {boolean} [log=true]
+	 */
+	notify(options, type, log) {
+		options.icon = __dirname + '/images/' + (type || 'notice') + '.png';
+		options.group = 1;
+
+		if (type == 'error' || type == 'fail') {
+			options.group = 2;
+			options.sound = true;
+			options.wait = true;
+		}
+
+		if (log !== false) {
+			console.log(options.message);
+		}
+
+		notifier.notify(options);
+
+		if (type == 'fail') {
+			process.exit(1);
+		}
+	},
+
+	/**
+	 * Log an error to the console
+	 *
+	 * @param {string} position
+	 * @param {string} message
+	 * @param {string} [details]
+	 */
+	logError(position, message, details) => {
+		console.log(
+			chalk.bgBlack.bold('[' + position + '] ') +
+			message + ' ' + (details || '')
+		);
+	}
+};
