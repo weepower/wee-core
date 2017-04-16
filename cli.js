63973e2323e5af8d8aa65dc73a422a5f14866365
Nathan Hood <nathanhood@me.com>
2017-04-16 15:50:13 -0500

    Update core cli to use commander
    - registering custom scripts will get added to —help print out for much easier cli use

--- cli.js
+++ cli.js
@@ -1,101 +1,89 @@
 /* global global, process */
 
-(function() {
-	'use strict';
+const fs = require('fs-extra'),
+	glob = require('glob'),
+	path = require('path'),
+	utils = require('./utils'),
 
-	var fs = require('fs-extra'),
-		glob = require('glob'),
-		path = require('path'),
-		utils = require('./utils'),
+	/**
+	 * Parse arguments from command array
+	 *
+	 * @param {Array} keywords
+	 * @returns {object}
+	 */
+	getArgs = function(keywords) {
+		var args = {};
 
-		/**
-		 * Parse arguments from command array
-		 *
-		 * @param {Array} keywords
-		 * @returns {object}
-		 */
-		getArgs = function(keywords) {
-			var args = {};
+		keywords.forEach(function(arg) {
+			if (arg.slice(0, 2) == '--') {
+				var segs = arg.slice(2).split('=');
+				args[segs[0]] = segs.length > 1 ? segs[1] : '';
+			}
+		});
 
-			keywords.forEach(function(arg) {
-				if (arg.slice(0, 2) == '--') {
-					var segs = arg.slice(2).split('=');
-					args[segs[0]] = segs.length > 1 ? segs[1] : '';
-				}
+		return args;
+	},
+
+	/**
+	 * Register all available commands
+	 *
+	 * @param {string} rootPath
+	 * @param {Array} paths
+	 * @returns {object}
+	 */
+	registerCommands = function(rootPath, paths) {
+		var commands = {};
+
+		paths.forEach(function(commandPath) {
+			var files = glob.sync(commandPath + '*.js');
+
+			files.forEach(function(file) {
+				var name = path.basename(file, '.js');
+				file = path.join(rootPath, file);
+
+				commands[name] = require(file);
 			});
+		});
 
-			return args;
-		},
-
-		/**
-		 * Register all available commands
-		 *
-		 * @param {string} rootPath
-		 * @param {Array} paths
-		 * @returns {object}
-		 */
-		registerCommands = function(rootPath, paths) {
-			var commands = {};
-
-			paths.forEach(function(commandPath) {
-				var files = glob.sync(commandPath + '*.js');
-
-				files.forEach(function(file) {
-					var name = path.basename(file, '.js');
-					file = path.join(rootPath, file);
-
-					commands[name] = function(config) {
-						require(file)(config);
-					};
-				});
-			});
-
-			return commands;
-		};
-
-	global.chalk = require('chalk');
-
-	module.exports = function(rootPath) {
-		var keywords = process.argv.slice(2),
-			keyCount = keywords.length,
-			args = getArgs(
-				keyCount === 1 ?
-					keywords.slice(0) :
-					keywords.slice(1)
-			),
-			configPath = path.join(rootPath, args.config || 'wee.json'),
-			project = JSON.parse(fs.readFileSync(configPath, 'utf8')),
-			cmd = project.defaultCommand || 'run';
-
-		// Process fallback command
-		if (keyCount > 1 || (keyCount && ! Object.keys(args).length)) {
-			cmd = keywords[0];
-		}
-
-		var parts = cmd.split(':'),
-			options = parts.slice(1),
-			commands = registerCommands(rootPath, [
-				'node_modules/wee-core/commands/',
-				path.join(project.paths.source, 'commands/')
-			]),
-			command = commands[parts[0]];
-
-		if (command) {
-			delete args.config;
-
-			command({
-				options: options,
-				args: args,
-				rootPath: rootPath,
-				project: project
-			});
-		} else {
-			utils.notify({
-				title: 'Command Error',
-				message: 'Command not found'
-			}, 'error');
-
-			process.exit();
-		}
+		return commands;
 	};
-})();
+
+global.chalk = require('chalk');
+
+module.exports = function(rootPath, program) {
+	var keywords = process.argv.slice(2),
+		keyCount = keywords.length,
+		args = getArgs(
+			keyCount === 1 ?
+				keywords.slice(0) :
+				keywords.slice(1)
+		),
+		configPath = path.join(rootPath, args.config || 'wee.json'),
+		project = JSON.parse(fs.readFileSync(configPath, 'utf8')),
+		commands = registerCommands(rootPath, [
+			'node_modules/wee-core/commands/',
+			path.join(project.paths.source, 'commands/')
+		]);
+
+	// Register commands
+	Object.keys(commands).forEach(name => {
+		let command = commands[name];
+
+		// TODO: Remove
+		if (! command.name) {
+			return;
+		}
+
+		let result = program.command(command.name)
+			.usage(command.usage || command.name)
+			.description(command.description || '');
+
+		if (command.options.length) {
+			command.options.forEach(option => {
+				result.option(option[0], option[1], option[2] || {});
+			});
+		}
+
+		result.action(command.action);
+	});
+};
