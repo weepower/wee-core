9734ea16a41e0e0fb2b84fbe6e1be2c04cf38498
Peter <peter@lewiscommunications.com>
2017-04-23 22:21:31 -0500

    Add parse url function

--- scripts/routes/index.js
+++ scripts/routes/index.js
@@ -1,20 +1,58 @@
+import { $unserialize } from 'core/types';
+import { _doc } from 'core/variables';
+
 const PARAMETER_REGEXP = /([:*])(\w+)/g;
 const WILDCARD_REGEXP = /\*/g;
 const REPLACE_VARIABLE_REGEXP = '([^\/]+)';
 const REPLACE_WILDCARD = '(?:.*)';
 const FOLLOWED_BY_SLASH_REGEXP = '(?:\/$|$)';
 const MATCH_REGEXP_FLAGS = '';
+const REMOVE_SLASHES_REGEXP = /^\/|\/$/g;
 let _routes = [];
 
-export function add(routes) {
+/**
+ * Add a route to routes array
+ *
+ * @private
+ * @param routes
+ */
+function _add(routes) {
 	routes.forEach(route => {
 		_routes.push(route);
 	});
 }
 
+/**
+ * Parse url and return results
+ *
+ * @private
+ * @param value
+ * @returns {{full: string, hash: (Blob|string|*|ArrayBuffer|Array.<T>|$), path: string, query: {}, segments: Array, url: (*|Location|String|string), port}}
+ * @private
+ */
+function _parseUrl(value) {
+	const a = _doc.createElement('a');
+	a.href = value || window.location;
+
+	const search = a.search,
+		path = a.pathname.replace(REMOVE_SLASHES_REGEXP, '');
+
+	return {
+		full: '/' + path + search + a.hash,
+		hash: a.hash.slice(1),
+		path: '/' + path,
+		query: search ? $unserialize(search) : {},
+		segments: path.split('/'),
+		url: a.href,
+		port: a.port
+	};
+}
+
 export default {
 	map(routes) {
-		add(routes);
+		_add(routes);
+
+		return this;
 	},
 	routes(index) {
 		if (index && _routes[index]) {
@@ -22,5 +60,11 @@ export default {
 		}
 
 		return _routes;
+	},
+	run() {
+		//
+	},
+	uri(value) {
+		return _parseUrl(value);
 	}
 };
