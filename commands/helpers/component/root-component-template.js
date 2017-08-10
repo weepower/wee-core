6edcea994a0981cac01d6d63226e91e3554592cc
Nathan Hood <nathanhood@me.com>
2017-08-10 13:32:59 -0500

    Update root component template

--- commands/helpers/component/root-component-template.js
+++ commands/helpers/component/root-component-template.js
@@ -1,14 +1,19 @@
 import { RouteHandler } from 'wee-routes';
 import Vue from 'vue';
-import ${variableName} from './index.vue';
+import ${variableName}Options from './index.vue';
 
 // Create component constructor
-export const ${constructorName} = Vue.extend(${variableName});
+export const ${constructorName} = Vue.extend(${variableName}Options);
+
+let ${variableName};
 
 // Register and mount component
 export default new RouteHandler({
 	init() {
 		// TODO: Set mounting selector
-		new ${constructorName}().$mount('.selector');
+		${variableName} = new ${constructorName}().$mount('.selector');
+	},
+	unload() {
+		${variableName}.$destroy();
 	}
 });
