5ce651027d342ada2e4162f0f1d1f9e6bbcf51b3
Nathan Hood <nathanhood@me.com>
2017-08-10 11:23:41 -0500

    Remove duplcate init property on RouteHandler

--- scripts/routes/route-handler.js
+++ scripts/routes/route-handler.js
@@ -3,7 +3,6 @@ import { genKey } from './key';
 export default class RouteHandler {
 	constructor(conf) {
 		this.id = genKey();
-		this.init = conf.init;
 		this.beforeInit = conf.beforeInit || null;
 		this.beforeUpdate = conf.beforeUpdate || null;
 		this.init = conf.init || null;
