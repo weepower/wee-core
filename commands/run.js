069b90d56e397e2738b8371e09f26484b8ff3e65
Peter Bedor <peter@lewiscommunications.com>
2018-02-27 15:47:30 -0600

    Add run command back

---
+++ commands/run.js
@@ -0,0 +1,48 @@
+const exec = require('child_process').exec;
+const spawn = require('child_process').spawn;
+const fs = require('fs-extra');
+const utils = require('../utils');
+
+module.exports = {
+    name: 'run',
+    description: 'run development server',
+    usage: '- wee run [options]',
+    options: [
+        ['-l, --local', 'wrap proxy around local dev domain'],
+        ['-s, --static', 'serve static files']
+    ],
+    action(config, options) {
+        let project = config.project,
+            projectUpdated = false;
+
+        if (options.local) {
+            if (!project.server.proxy) {
+                utils.logError('Set proxy domain in wee.json');
+                process.exit();
+            }
+
+            project.server.static = false;
+            projectUpdated = true;
+        }
+
+        if (options.static && !project.server.static) {
+            project.server.static = true;
+            projectUpdated = true;
+        }
+
+        // Update wee.json
+        if (projectUpdated) {
+            fs.writeJsonSync(config.rootPath + '/wee.json', project, { spaces: '\t' });
+        }
+
+        // Execute npm run
+        let child = spawn('npm', ['start'], {
+            cwd: config.rootPath,
+            stdio: 'inherit'
+        });
+
+        child.on('error', data => {
+            utils.logError(data);
+        });
+    }
+};
