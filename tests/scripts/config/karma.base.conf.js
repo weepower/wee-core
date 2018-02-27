b7f6c8cdb1aff634325ba6c7e042e627cb7c5da0
Peter Bedor <peter@lewiscommunications.com>
2018-02-27 15:44:09 -0600

    Adjust karma base for saucelabs

--- tests/scripts/config/karma.base.conf.js
+++ tests/scripts/config/karma.base.conf.js
@@ -1,15 +1,10 @@
-const fs = require('fs-extra');
 const glob = require('glob');
+const fs = require('fs-extra');
 const paths = require('../../../utils').paths;
-const args = process.argv.filter(arg => arg.includes('--')).map(arg => {
-	return arg.replace('--', '').split(/=| +/).map(word => word.trim());
-});
-let spec = args.find(arg => arg[0] === 'spec');
-let reporters = ['mocha', 'coverage'];
-let files = [];
-let preprocessors = [];
 
-spec = Array.isArray(spec) ? spec[1] : '';
+const reporters = ['mocha', 'coverage'];
+const files = [];
+const preprocessors = [];
 
 /**
  * Add file for processing
@@ -23,14 +18,7 @@ function addFile(file) {
 	preprocessors[file] = ['webpack'];
 }
 
-// Remove coverage if running one spec file
-// as it makes coverage inaccurate
-if (spec.length) {
-	reporters.pop();
-	addFile(paths.tests.scripts + '/unit/' + spec);
-} else {
-	glob.sync(paths.tests.scripts + '/unit/**/*.js').forEach(file => addFile(file));
-}
+glob.sync(paths.tests.scripts + '/unit/**/*.js').forEach(file => addFile(file));
 
 // Inject test file for assets module
 files.push({
@@ -44,57 +32,26 @@ module.exports = {
 	basePath: '../..',
 
 	proxies: {
-		'/files': '/base/scripts/helpers/files/'
+		'/files/': '/base/scripts/helpers/files/'
 	},
 
-	// list of files / patterns to load in the browser
-	files,
-
-	// Process files before serving to browser
-	// karma-coverage not registered here because of babel-plugin-istanbul
-	preprocessors,
-
-	// frameworks to use
-	// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
 	frameworks: ['mocha', 'chai'],
-	webpackMiddleware: {
-		noInfo: true
-	},
+
 	webpack: require('./webpack.config.js'),
-
-	// Reporters
-	reporters: reporters,
-	mochaReporter: {
-		showDiff: true
-	},
-	coverageReporter: {
-		reporters: [
-			{
-				type: 'lcovonly',
-				dir: '../../../coverage',
-				subdir: '.'
-			},
-			{
-				type: 'text'
-			}
-		]
-	},
-
-	// web server port
-	protocol: 'http',
-	port: 9876,
-	httpsServerOptions: {
-		// key: fs.readFileSync(resolve(__dirname, './https/server.key'), 'utf8'),
-		// cert: fs.readFileSync(resolve(__dirname, './https/server.crt'), 'utf8')
-	},
-
-	// enable / disable colors in the output (reporters and logs)
-	colors: true,
 
 	// Concurrency level
 	// how many browser should be started simultaneous
 	concurrency: Infinity,
 
-	// This is a hack to keep karma from re-running tests over and over
-	browserNoActivityTimeout: 2000
+	captureTimeout: 120000,
+
+	browserNoActivityTimeout: 120000,
+
+	webpackMiddleware: {
+		noInfo: true
+	},
+
+	files,
+	preprocessors,
+	reporters,
 };
