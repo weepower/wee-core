56a28dec824205743ce75721f9e1b75c180f4025
peterbedor <peterbedor@gmail.com>
2018-09-11 23:09:36 -0500

    Adjust guid function

--- scripts/mediator/guid.js
+++ scripts/mediator/guid.js
@@ -1,7 +1,5 @@
-export default function guidGenerator() {
-    const S4 = function () {
-        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-    };
+const S4 = () => (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
 
+export default function guidGenerator() {
     return `${S4()}${S4()}-${S4()}-${S4()}-${S4()}-${S4()}${S4()}${S4()}`;
 }
